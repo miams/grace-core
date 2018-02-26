14d8cb2f1a2f73525219647a05e39735462b711f
Aidan Feldman <aidan.feldman@gsa.gov>
2018-02-26 16:03:06 -0500

    include documentation around the apply script

--- apply.py
+++ apply.py
@@ -4,9 +4,9 @@ import subprocess
 import sys
 
 
-MODULE = 'test'
+MODULE = os.getenv('MODULE', 'terraform')
 # https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_accounts_access.html#orgs_manage_accounts_access-cross-account-role
-ROLE_NAME = 'OrganizationAccountAccessRole'
+ROLE_NAME = os.getenv('ROLE_NAME', 'OrganizationAccountAccessRole')
 
 
 def get_current_account_id():
