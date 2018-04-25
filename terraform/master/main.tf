bdcf422bf0de6b8b5c6e9a55b5abcb4ba36f3543
Aidan Feldman <aidan.feldman@gsa.gov>
2018-04-25 17:43:54 -0400

    include another subaccount, for full proof of concept

--- terraform/master/main.tf
+++ terraform/master/main.tf
@@ -10,3 +10,10 @@ module "scp_test" {
   name  = "Aidan SCP test"
   email = "aidan.feldman+scp@gsa.gov"
 }
+
+module "broker_test" {
+  source = "./child_account"
+
+  name  = "Service Broker account"
+  email = "aidan.feldman+broker@gsa.gov"
+}
