adecc4bc5858484a1ccf026f248576091b58e019
Jason Miller <jmiller@red-abstract.com>
2018-05-24 10:40:50 -0500

    Just reorganized variables

--- terraform/networking/sg-nacls-vars.tf
+++ terraform/networking/sg-nacls-vars.tf
@@ -1,3 +1,5 @@
+# Env VPC variables
+
 variable "vpc_env_set_default_ingress_https_rule" {
   type    = "string"
   default = "true"
@@ -13,27 +15,12 @@ variable "vpc_env_set_default_egress_rule" {
   default = "true"
 }
 
-variable "vpc_mgmt_set_default_ingress_rdp_ssh_rule" {
-  type    = "string"
-  default = "true"
-}
-
-variable "vpc_mgmt_set_default_egress_rule" {
-  type    = "string"
-  default = "true"
-}
-
 variable "vpc_env_set_default_public_nacl" {
   type    = "string"
   default = "true"
 }
 
 variable "vpc_env_set_default_private_nacl" {
-  type    = "string"
-  default = "true"
-}
-
-variable "vpc_mgmt_set_default_private_nacl" {
   type    = "string"
   default = "true"
 }
@@ -45,11 +32,6 @@ variable "vpc_env_ingress_https_sg_name" {
 variable "vpc_env_ingress_https_cidr" {
   default = "0.0.0.0/0"
   type    = "string"
-}
-
-variable "vpc_mgmt_ec2_management_sg_name" {
-  type    = "string"
-  default = "mgmt_ec2_management_sg"
 }
 
 variable "vpc_env_ec2_management_sg_name" {
@@ -66,18 +48,6 @@ variable "env_ec2_egress_on_prem_smtp_cidrs" {
 }
 
 variable "env_ec2_egress_on_prem_all_traffic_cidrs" {
-  type = "list"
-}
-
-variable "vpc_mgmt_ec2_egress_on_prem_sg_name" {
-  type = "string"
-}
-
-variable "mgmt_ec2_egress_on_prem_smtp_cidrs" {
-  type = "list"
-}
-
-variable "mgmt_ec2_egress_on_prem_all_traffic_cidrs" {
   type = "list"
 }
 
@@ -107,6 +77,27 @@ variable "env_sg_ingress_ssh_protocol" {
 
 variable "env_sg_ingress_ssh_cidrs" {
   type = "list"
+}
+
+# Management VPC variables
+variable "vpc_mgmt_set_default_ingress_rdp_ssh_rule" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_mgmt_set_default_egress_rule" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_mgmt_set_default_private_nacl" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_mgmt_ec2_management_sg_name" {
+  type    = "string"
+  default = "mgmt_ec2_management_sg"
 }
 
 variable "mgmt_sg_ingress_rdp_port" {
@@ -142,5 +133,17 @@ variable "mgmt_private_nacl_ingress_cidrs" {
 }
 
 variable "mgmt_private_nacl_egress_cidrs" {
+  type = "list"
+}
+
+variable "vpc_mgmt_ec2_egress_on_prem_sg_name" {
+  type = "string"
+}
+
+variable "mgmt_ec2_egress_on_prem_smtp_cidrs" {
+  type = "list"
+}
+
+variable "mgmt_ec2_egress_on_prem_all_traffic_cidrs" {
   type = "list"
 }
