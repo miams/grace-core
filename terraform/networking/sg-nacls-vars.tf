9cf8f54813a410c6b111a3e0f74a34854376ca91
Jason Miller <jmiller@red-abstract.com>
2018-05-24 10:07:53 -0500

    Implementing default SG/NACL code

---
+++ terraform/networking/sg-nacls-vars.tf
@@ -0,0 +1,146 @@
+variable "vpc_env_set_default_ingress_https_rule" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_env_set_default_ingress_rdp_ssh_rule" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_env_set_default_egress_rule" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_mgmt_set_default_ingress_rdp_ssh_rule" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_mgmt_set_default_egress_rule" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_env_set_default_public_nacl" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_env_set_default_private_nacl" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_mgmt_set_default_private_nacl" {
+  type    = "string"
+  default = "true"
+}
+
+variable "vpc_env_ingress_https_sg_name" {
+  type = "string"
+}
+
+variable "vpc_env_ingress_https_cidr" {
+  default = "0.0.0.0/0"
+  type    = "string"
+}
+
+variable "vpc_mgmt_ec2_management_sg_name" {
+  type    = "string"
+  default = "mgmt_ec2_management_sg"
+}
+
+variable "vpc_env_ec2_management_sg_name" {
+  type    = "string"
+  default = "env_ec2_management_sg"
+}
+
+variable "vpc_env_ec2_egress_on_prem_sg_name" {
+  type = "string"
+}
+
+variable "env_ec2_egress_on_prem_smtp_cidrs" {
+  type = "list"
+}
+
+variable "env_ec2_egress_on_prem_all_traffic_cidrs" {
+  type = "list"
+}
+
+variable "vpc_mgmt_ec2_egress_on_prem_sg_name" {
+  type = "string"
+}
+
+variable "mgmt_ec2_egress_on_prem_smtp_cidrs" {
+  type = "list"
+}
+
+variable "mgmt_ec2_egress_on_prem_all_traffic_cidrs" {
+  type = "list"
+}
+
+variable "env_sg_ingress_rdp_port" {
+  type    = "string"
+  default = "3389"
+}
+
+variable "env_sg_ingress_rdp_protocol" {
+  type    = "string"
+  default = "tcp"
+}
+
+variable "env_sg_ingress_rdp_cidrs" {
+  type = "list"
+}
+
+variable "env_sg_ingress_ssh_port" {
+  type    = "string"
+  default = "22"
+}
+
+variable "env_sg_ingress_ssh_protocol" {
+  type    = "string"
+  default = "tcp"
+}
+
+variable "env_sg_ingress_ssh_cidrs" {
+  type = "list"
+}
+
+variable "mgmt_sg_ingress_rdp_port" {
+  type    = "string"
+  default = "3389"
+}
+
+variable "mgmt_sg_ingress_rdp_protocol" {
+  type    = "string"
+  default = "tcp"
+}
+
+variable "mgmt_sg_ingress_rdp_cidrs" {
+  type = "list"
+}
+
+variable "mgmt_sg_ingress_ssh_port" {
+  type    = "string"
+  default = "22"
+}
+
+variable "mgmt_sg_ingress_ssh_protocol" {
+  type    = "string"
+  default = "tcp"
+}
+
+variable "mgmt_sg_ingress_ssh_cidrs" {
+  type = "list"
+}
+
+variable "mgmt_private_nacl_ingress_cidrs" {
+  type = "list"
+}
+
+variable "mgmt_private_nacl_egress_cidrs" {
+  type = "list"
+}
--- terraform/networking/vpc-mgmt.tf
+++ terraform/networking/vpc-mgmt.tf
@@ -42,3 +42,98 @@ resource "aws_vpc_peering_connection_accepter" "peer_vpc_env" {
     Side = "Accepter for the environment"
   }
 }
+
+# TODO: Add conditional with default of true for this
+resource "aws_security_group" "mgmt_ec2_management_sg" {
+  # Conditionally create this resource if value is true
+  count = "${var.vpc_mgmt_set_default_ingress_rdp_ssh_rule == "true" ? 1 : 0}"
+
+  name        = "${var.vpc_mgmt_ec2_management_sg_name}"
+  description = "Tenant Security Group for managing instances"
+
+  providers = {
+    aws = "aws.mgmt"
+  }
+
+  vpc_id = "${module.vpc_mgmt.vpc_id}"
+
+  ingress {
+    from_port   = "${var.mgmt_sg_ingress_rdp_port}"
+    to_port     = "${var.mgmt_sg_ingress_rdp_port}"
+    protocol    = "${var.mgmt_sg_ingress_rdp_protocol}"
+    cidr_blocks = "${var.mgmt_sg_ingress_rdp_cidrs}"
+  }
+
+  ingress {
+    from_port   = "${var.mgmt_sg_ingress_ssh_port}"
+    to_port     = "${var.mgmt_sg_ingress_ssh_port}"
+    protocol    = "${var.mgmt_sg_ingress_ssh_protocol}"
+    cidr_blocks = "${var.mgmt_sg_ingress_ssh_cidrs}"
+  }
+
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+}
+
+resource "aws_security_group" "mgmt_ec2_egress_on_prem" {
+  count = "${var.vpc_mgmt_set_default_egress_rule == "true" ? 1 : 0}"
+
+  name        = "${var.vpc_mgmt_ec2_egress_on_prem_sg_name}"
+  description = "Tenant security group for egress to on-prem"
+
+  providers = {
+    aws = "aws.mgmt"
+  }
+
+  vpc_id = "${module.vpc_mgmt.vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = "${var.mgmt_ec2_egress_on_prem_all_traffic_cidrs}"
+  }
+
+  egress {
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = "${var.mgmt_ec2_egress_on_prem_smtp_cidrs}"
+  }
+}
+
+# TODO: NACL for private subnets
+
+resource "aws_network_acl" "mgmt_private_nacl" {
+  # TODO: Possible problem here with count as a conditional, need to test.
+  count = "${var.vpc_mgmt_set_default_private_nacl == "true" ? 1 : 0}"
+
+  vpc_id     = "${module.vpc_mgmt.vpc_id}"
+  subnet_ids = "${module.vpc_mgmt.private_subnets}"
+
+  providers = {
+    aws = "aws.mgmt"
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "${var.mgmt_private_nacl_ingress_cidrs}"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "${var.mgmt_private_nacl_egress_cidrs}"
+    from_port  = 0
+    to_port    = 0
+  }
+}
