71ec67fc366c6553a1aced1270a364c4a36aaa1c
Aidan Feldman <aidan.feldman@gsa.gov>
2018-05-24 23:38:03 -0400

    set the transit VPC bucket policy for all child accounts

--- terraform/master/main.tf
+++ terraform/master/main.tf
@@ -7,3 +7,5 @@ terraform {
     region = "us-east-1"
   }
 }
+
+data "aws_caller_identity" "current" {}
--- terraform/master/transit_vpc.tf
+++ terraform/master/transit_vpc.tf
@@ -16,7 +16,9 @@ resource "aws_cloudformation_stack" "transit_vpc" {
   template_body = "${file("${path.module}/files/transit_vpc_template.json")}"
 
   parameters {
-    AccountId  = "${module.tenant_1_dev.account_id}"
+    # arbitrary, since it will be overwritten below
+    AccountId = "${module.tenant_1_dev.account_id}"
+
     KeyName    = "Cisco-CSR-Transit-VPC-Grace"
     PubSubnet1 = "${var.transit_vpc_subnet_1_cidr}"
     PubSubnet2 = "${var.transit_vpc_subnet_2_cidr}"
@@ -26,4 +28,39 @@ resource "aws_cloudformation_stack" "transit_vpc" {
   lifecycle {
     prevent_destroy = true
   }
+}
+
+# This will override the bucket policy created by the CloudFormation stack. Needed because only one account ID can be passed in as a parameter.
+# https://docs.aws.amazon.com/solutions/latest/cisco-based-transit-vpc/appendix-c.html
+resource "aws_s3_bucket_policy" "transit_vpc" {
+  provider = "aws.netops"
+
+  bucket = "${aws_cloudformation_stack.transit_vpc.outputs.ConfigS3Bucket}"
+
+  policy = <<POLICY
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::${module.tenant_1_staging.account_id}:root",
+                    "arn:aws:iam::${module.devsecops_test_3.account_id}:root",
+                    "arn:aws:iam::${module.tenant_1_mgmt.account_id}:root",
+                    "arn:aws:iam::${module.tenant_1_prod.account_id}:root",
+                    "arn:aws:iam::${module.tenant_1_dev.account_id}:root",
+                    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+                ]
+            },
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:PutObjectAcl"
+            ],
+            "Resource": "arn:aws:s3:::${aws_cloudformation_stack.transit_vpc.outputs.ConfigS3Bucket}/${aws_cloudformation_stack.transit_vpc.outputs.BucketPrefix}*"
+        }
+    ]
+}
+POLICY
 }
