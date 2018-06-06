3f269d255ad36cf2fffae78dfc216140b903d475
Jason Miller <jmiller@red-abstract.com>
2018-06-06 09:38:42 -0500

    Remove unnecessary comments

--- terraform/member_account/iam_roles.tf
+++ terraform/member_account/iam_roles.tf
@@ -1,21 +1,3 @@
-# TODO: Need to find a way to dynamically build the ARN just from a list of usernames provided below, so the principal ends up with a list of ARNs and not a list of usernames - can I use a null_resource to send in a username and have it return an ARN with the username and authlanding_account_id (see authlanding_account.tf for possible example of building a list and figure out how to get the full list of ARNs into the sts_assume_role policy on IAM creation - ideas: element, concat, count, etc.
-# https://blog.gruntwork.io/terraform-tips-tricks-loops-if-statements-and-gotchas-f739bbae55f9
-# THIS LOOKS LIKE A WINNER (jsonencode): https://stackoverflow.com/questions/43513943/how-does-one-combine-concat-with-formatlist-in-terraform/43526664
-
-# {
-#     "Action": [
-#         "s3:Get*",
-#         "s3:List*"
-#     ],
-#    "Effect": "Allow",
-#    "Resource": ${jsonencode(
-#      concat(
-#        formatlist("arn:aws:s3:::%s", var.data_pipeline_s3_buckets),
-#        formatlist("arn:aws:s3:::%s/", var.data_pipeline_s3_buckets)
-#      )
-#    )}
-# }
-
 resource "aws_iam_role" "tenant_admin_role" {
   count = "${var.create_iam_roles == "true" ? 1 : 0}"
 
