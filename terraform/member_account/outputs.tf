ea56d30446fc2b47408e2e3c48a65e892775d9f8
Jason Miller <jmiller@red-abstract.com>
2018-06-27 23:26:52 -0500

    Adding outputs to the member account module

--- terraform/member_account/outputs.tf
+++ terraform/member_account/outputs.tf
@@ -5,3 +5,23 @@ output "account_id" {
 output "root_arn" {
   value = "arn:aws:iam::${aws_organizations_account.child.id}:root"
 }
+
+output "tenant_admin_role_arn" {
+  value = "${aws_iam_role.tenant_admin_role.arn}"
+}
+
+output "tenant_power_user_role_arn" {
+  value = "${aws_iam_role.tenant_power_user_role.arn}"
+}
+
+output "tenant_view_only_role_arn" {
+  value = "${aws_iam_role.tenant_view_only_role.arn}"
+}
+
+output "tenant_secops_admin_role_arn" {
+  value = "${aws_iam_role.secops_admin_role.arn}"
+}
+
+output "tenant_secops_view_only_role_arn" {
+  value = "${aws_iam_role.secops_view_only_role.arn}"
+}
