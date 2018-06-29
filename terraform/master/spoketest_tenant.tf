96ac60c405ba6427716385a2b84f6322d1c8fabe
Jason Miller <jmiller@red-abstract.com>
2018-06-29 14:14:10 -0500

    IAM policies in spoketest

--- terraform/master/spoketest_tenant.tf
+++ terraform/master/spoketest_tenant.tf
@@ -98,3 +98,249 @@ module "spoketest_budget" {
     "${module.tenant_spoketest_dev.account_id}",
   ]
 }
+
+# Apply IAM roles to the users to allow them to assume the necessary roles. This should only be run if create-iam-roles == true
+# Now we need to read the role list and interate through the named users and apply an inline sts-assume-role policy
+# Admin roles first
+
+resource "aws_iam_policy" "sts_assume_admin_role_user_policy_mgmt" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_mgmt_admin_assume_role_user_policy_mgmt"
+  description = "Allows this user to assume the admin role in this tenant mgmt account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_mgmt.tenant_admin_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_admin_policy_mgmt" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_admin_iam_role_list)}"
+  user       = "${local.spoketest_tenant_admin_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_admin_role_user_policy_mgmt.arn}"
+}
+
+resource "aws_iam_policy" "sts_assume_admin_role_user_policy_prod" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_prod_admin_assume_role_user_policy_prod"
+  description = "Allows this user to assume the admin role in this tenant prod account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_prod.tenant_admin_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_admin_policy_prod" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_admin_iam_role_list)}"
+  user       = "${local.spoketest_tenant_admin_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_admin_role_user_policy_prod.arn}"
+}
+
+resource "aws_iam_policy" "sts_assume_admin_role_user_policy_dev" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_dev_admin_assume_role_user_policy_dev"
+  description = "Allows this user to assume the admin role in this tenant dev account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_dev.tenant_admin_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_admin_policy_dev" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_admin_iam_role_list)}"
+  user       = "${local.spoketest_tenant_admin_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_admin_role_user_policy_dev.arn}"
+}
+
+###
+# PowerUser roles
+###
+
+resource "aws_iam_policy" "sts_assume_poweruser_role_user_policy_mgmt" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_mgmt_poweruser_assume_role_user_policy_mgmt"
+  description = "Allows this user to assume the poweruser role in this tenant mgmt account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_mgmt.tenant_poweruser_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_poweruser_policy_mgmt" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_poweruser_iam_role_list)}"
+  user       = "${local.spoketest_tenant_poweruser_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_poweruser_role_user_policy_mgmt.arn}"
+}
+
+resource "aws_iam_policy" "sts_assume_poweruser_role_user_policy_prod" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_prod_poweruser_assume_role_user_policy_prod"
+  description = "Allows this user to assume the poweruser role in this tenant prod account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_prod.tenant_poweruser_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_poweruser_policy_prod" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_poweruser_iam_role_list)}"
+  user       = "${local.spoketest_tenant_poweruser_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_poweruser_role_user_policy_prod.arn}"
+}
+
+resource "aws_iam_policy" "sts_assume_poweruser_role_user_policy_dev" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_dev_poweruser_assume_role_user_policy_dev"
+  description = "Allows this user to assume the poweruser role in this tenant dev account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_dev.tenant_poweruser_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_poweruser_policy_dev" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_poweruser_iam_role_list)}"
+  user       = "${local.spoketest_tenant_poweruser_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_poweruser_role_user_policy_dev.arn}"
+}
+
+###
+# ViewOnly Roles
+###
+
+resource "aws_iam_policy" "sts_assume_viewonly_role_user_policy_mgmt" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_mgmt_viewonly_assume_role_user_policy_mgmt"
+  description = "Allows this user to assume the viewonly role in this tenant mgmt account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_mgmt.tenant_viewonly_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_viewonly_policy_mgmt" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_viewonly_iam_role_list)}"
+  user       = "${local.spoketest_tenant_viewonly_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_viewonly_role_user_policy_mgmt.arn}"
+}
+
+resource "aws_iam_policy" "sts_assume_viewonly_role_user_policy_prod" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_prod_viewonly_assume_role_user_policy_prod"
+  description = "Allows this user to assume the viewonly role in this tenant prod account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_prod.tenant_viewonly_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_viewonly_policy_prod" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_viewonly_iam_role_list)}"
+  user       = "${local.spoketest_tenant_viewonly_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_viewonly_role_user_policy_prod.arn}"
+}
+
+resource "aws_iam_policy" "sts_assume_viewonly_role_user_policy_dev" {
+  provider    = "aws.authlanding"
+  name        = "tenant_spoketest_dev_viewonly_assume_role_user_policy_dev"
+  description = "Allows this user to assume the viewonly role in this tenant dev account"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "${module.tenant_spoketest_dev.tenant_viewonly_role_arn}",
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_user_viewonly_policy_dev" {
+  provider   = "aws.authlanding"
+  count      = "${length(local.spoketest_tenant_viewonly_iam_role_list)}"
+  user       = "${local.spoketest_tenant_viewonly_iam_role_list[count.index]}"
+  policy_arn = "${aws_iam_policy.sts_assume_viewonly_role_user_policy_dev.arn}"
+}
